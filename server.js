a82c8f4e3fef5388e2a3eee04fbc97583b1d1d80
Salvatore <mammoliti.sam@gmail.com>
2024-02-08 11:23:38 -0500

    created server.js file. added and imported require dependancy, function to initialize express, port variable for listening, middlewares and server listener function"

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+// Import dependancies
 const express = require('express');
 
+// Function to initialize express
 const app = express();
 
+// PORT variable to determine which port express will listen on
 const PORT = process.env.PORT || 3001;
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-const apiRoutes = require("./routes")
+const apiRoutes = require("./routes/api-routes/apiRoutes");
+app.use(apiRoutes);
+const htmlRoutes = require("./routes/html-routes/htmlRoutes")
+app.use(htmlRoutes);
+
+// Server listener
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
