f210d68802f8c8956003348c121287087988dac6
Salvatore <mammoliti.sam@gmail.com>
2024-02-08 11:11:04 -0500

    "installed uuid to generate unique note id's. created server.js file. created apiRoutes and htmlRoutes folders and .js files for both routes"

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+const apiRoutes = require("./routes")
