d6457199fe06ea2c5b1a2f398a58a6e04a30c0e2
Salvatore <mammoliti.sam@gmail.com>
2024-02-08 12:45:13 -0500

    "renamed db.json to database.json and db.js to database to avoid confusion with folder name which is db. added to do comments list to apiRoutes.js. added uuid import varibale to assign notes unque id and database import variable to link class which will be created later"

--- routes/api-routes/apiRoutes.js
+++ routes/api-routes/apiRoutes.js
@@ -1,2 +1,33 @@
 const express = require('express');
 const router = require('router');
+
+
+const uuid = require('uuid');
+
+
+const database = require('../db/database');
+
+
+
+// function to GET api notes  that will read database.json file and return
+// all saved notes as json data
+
+
+// function to POST api notes that are recieved from new note added in database.json file
+//  then add it to database.json then return new note to client
+
+
+// add unique id to each note when saved. do old notes need id as well?
+
+// BONUS add DELETE api/notes/:id should recieve a query param containing the id of a note to 
+// delete. in order to delete a note, you'll need to read all notes from the database.json file
+// remove the note with the given id property and then rewrite the note(s) to the database.json file
+
+
+
+
+
+
+module.exports = router
+
+
